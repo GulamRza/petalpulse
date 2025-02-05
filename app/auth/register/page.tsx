667a0561d443e85b494b0cb9e6e1b3c9bed93a20
gulamrza <gulamrza000@gmail.com>
2025-02-05 11:09:17 +0530

    updated register page to server component

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,49 +1,40 @@
-"use client";
-
 import Image from "next/image";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { prisma } from "@/app/utils/prisma";
+import { redirect } from "next/navigation";
 
+const RegisterPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<any>;
+}) => {
+  const { error } = await searchParams;
+  const users = await prisma.user.findMany({});
+  console.log(users);
+  // revalidatePath("auth/register?error=Failed,%20Try%20Again");
 
-const RegisterPage = () => {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const router = useRouter();
-
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (formData: FormData) => {
+    "use server";
     // Add your registration logic here
 
+    // return;
 
-    const response = await fetch('/api/user', {
-      method: "POST",
-      body: JSON.stringify({
-        name, email, password
-      })
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const repassword = formData.get("repassword") as string;
+
+    if (name == "" || email == "" || password == "" || password != repassword)
+      redirect("/auth/register?error=Invalid%20Inputs");
+
+    const user = await prisma.user.create({
+      data: { name, email, password },
     });
 
-    if (response.ok){
-      const user = (await response.json()).user;
-
-      if (!user){
-        alert('Unable to register. Please Retry')
-        setName('')
-        setEmail('')
-        setPassword('')
-      }else{
-        router.push('/auth/sign-in');
-      }
-    }else{
-      alert('Unable to register. Please Retry')
-      setName('')
-      setEmail('')
-      setPassword('')  
+    if (user) {
+      redirect("/auth/sign-in");
+    } else {
+      redirect("/auth/register?error=Failed,%20Try%20Again");
     }
-
   };
 
   return (
@@ -51,8 +42,8 @@ const RegisterPage = () => {
       <div className="flex flex-col md:flex-row shadow-lg lg:rounded-xl bg-white dark:bg-gray-900 overflow-hidden lg:w-11/12 max-w-5xl">
         {/* Left Section */}
         <div className="flex-1 flex flex-col justify-center items-center p-8 bg-gradient-to-br from-green-500 to-green-700 text-white dark:from-green-700 dark:to-green-900 relative">
-          <Image 
-            src={'/green-leaves-bg.jpg'}
+          <Image
+            src={"/green-leaves-bg.jpg"}
             width={400}
             height={400}
             alt="sign-in-background"
@@ -69,10 +60,7 @@ const RegisterPage = () => {
           <h2 className="text-2xl font-semibold text-green-700 dark:text-green-300 mb-6">
             Create your account
           </h2>
-          <form
-            className="flex flex-col space-y-4"
-            onSubmit={handleSubmit}
-          >
+          <form className="flex flex-col space-y-4" action={handleSubmit}>
             {/* Name Input */}
             <div>
               <label
@@ -85,7 +73,7 @@ const RegisterPage = () => {
                 id="name"
                 type="text"
                 placeholder="Enter your full name"
-                onChange={(e) => setName(e.target.value)}
+                name="name"
                 className="w-full mt-1 p-3 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
               />
             </div>
@@ -101,7 +89,7 @@ const RegisterPage = () => {
               <input
                 id="email"
                 type="email"
-                onChange={(e) => setEmail(e.target.value)}
+                name="email"
                 placeholder="Enter your email"
                 className="w-full mt-1 p-3 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
               />
@@ -118,7 +106,7 @@ const RegisterPage = () => {
               <input
                 id="password"
                 type="password"
-                onChange={(e) => setPassword(e.target.value)}
+                name="password"
                 placeholder="Enter your password"
                 className="w-full mt-1 p-3 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
               />
@@ -135,6 +123,7 @@ const RegisterPage = () => {
               <input
                 id="confirmPassword"
                 type="password"
+                name="repassword"
                 placeholder="Confirm your password"
                 className="w-full mt-1 p-3 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
               />
@@ -148,6 +137,11 @@ const RegisterPage = () => {
               Register
             </button>
           </form>
+
+          {/* error if any */}
+          <div className="mt-6 text-center">
+            <p className="text-sm text-red-500 dark:text-red-400">{error}</p>
+          </div>
 
           {/* Links */}
           <div className="mt-6 text-center">
